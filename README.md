c7a839f0de50dcc8942013e3773a7dcde6e539e4
vanadiumoxide <vitosgh@gmail.com>
2020-09-05 11:46:24 +0700

    Added location selector, improved readability

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # BIgDoG
 
-What is **BIgDoG** anyway?\
-**BIgDoG** is an abbreviation of **B**asic **I**ma**g**e **Do**wnloader for BMK**G** site, while BMKG is a national
- agency for meteorology, climatology, and geophysics in Indonesia. Its function is to download radar images from the BMKG site. This project is in very early development.
+What is **BIgDoG** ?\
+**BIgDoG** is an abbreviation of **B**asic **I**ma**g**e **Do**wnloader for BMK**G** site. The BMKG itself is a national agency for meteorology, climatology, and geophysics in Indonesia. The purpose of this script is to download radar images from the BMKG site periodically. This project is in early development and might be abandoned.
 
 ## More Information
+### Libraries used
+Libraries used: datetime, hashlib, json, os, requests, time.
+
+These libraries are built-in. No need to download any suplementary libraries.
+
 ### Requirements
-The script is written in Python 3.8 and there are f-strings inside. Therefore, you need minimum of Python 3.6 
-interpreter to run this.
+The downloader.py was written in Python 3 and f-strings are used. The f-strings are available since Python version 3.6. Therefore, you need Python 3.6 or later.
 
 ### Explanation
-What happens if you run this script?
-1) It downloads an image from [this site](https://dataweb.bmkg.go.id/MEWS/Radar/TANG_SingleLayerCRefQC.png) 
-(All images are copyright to their respective owners). Brief description:
+What will happen if you run this script?
+1) Immediately downloads an image from the location you chosen. Base site: [Citra radar BMKG](https://www.bmkg.go.id/cuaca/citra-radar.bmkg) 
+ Brief description:
 
-    > Site shows a radar image of rain on the Jabodetabek area. It refreshes at a random interval between 3-10 minutes.
-2) The hash of the site will be checked using hashlib module. It helps to differentiate the image when the site 
-is checked again.
-3) Script sleeps for 2 minutes. After that, the script will run again to check whether the image is updated or not by 
-hashing the site. If the image appears to be different from the previous one, the new one will be downloaded.
+    > Site shows radar images of rain at certain area. The site is updating its images at random interval between 3-10 minutes.
+2) The hash of the image will be checked by hashlib. This step helps to recognize the image.
+3) Downloader.py will sleep for 2 minutes. From there on, the script will run again to check whether the image is updated or not by hashing the image. If the image appears to be different from the previous image, the new one will be downloaded.
 4) This script will repeat the cycle until it's forcibly closed.
 
 ## Contributing
--- downloader.py
+++ downloader.py
@@ -1,102 +1,108 @@
-# BIgDoG: Basic Image Downloader for BMKG site.
-# This script is intended to download radar images from BMKG website.
-# All images are copyright to their respective owners.
-# Requires Python 3.6 or later for f-strings.
+# BIgDoG: Basic Image Downloader for BMKG site
+# This script is intended to download radar images from BMKG website
+# For personal use only
+# Use Python version 3.6 or later
 from datetime import datetime
 import hashlib
+import json
 import os
 import requests
 import time
 
 # Variable assignment
-# Site source
-default_url = "https://dataweb.bmkg.go.id/MEWS/Radar/TANG_SingleLayerCRefQC.png"
-# Time interval between the checking session in second
-t_interval = 120
-# For image numbering
 count = 1
-# Get the time today
-dt_now = datetime.now().strftime("%b %d %Y %H %M")
-# To make directory with current time as the name e.g. Mar 31, 2020 21 30
-img_dir = f"{os.getcwd()}\\{dt_now}"
+DT_NOW = datetime.now().strftime("%b %d %Y %H %M")  
+IMG_DIR = f"{os.getcwd()}\\{DT_NOW}"
+
+def get_data():
+    with open("region.json", "r") as region:
+        data = json.loads(region.read())
+    return data
 
 
-# Output in command line
-def print_intro():
-    print("\n\n", "=" * 78, sep='')
-    print("This is an automated script named BIgDoG (Basic Image Downloader"
-          " for BMKG site).")
-    print("Location: Tangerang.")
-    print("There is no way to change the location or site unless the script"
-          " is modified.")
-    print("=" * 78, "\n")
-    print(f"Creating directory {dt_now}.")
+def prompt_location():
+    while True:
+        change_location = input("Change current location? (Y/N)\n")
+        if change_location.upper() == "Y":
+            return True
+        elif change_location.upper() == "N":
+            return False
+        print("Invalid input!")
 
-
-# Make the directory with a name as seen from above
-def make_directory():
+    
+def make_directory(location):
+    print(f"Creating directory '{DT_NOW} {location}'.")
     try:
-        os.mkdir(f"{img_dir}")
+        os.mkdir(f"{IMG_DIR} {location}")
     except OSError:
-        # If it fails, this script terminates
         print("Failed to create directory!\nDoes the script have access to write a directory?")
         exit()
     else:
-        # Looking good!
         print("Directory created successfully!\n")
 
 
-# Function to get the content of site
-def img_content():
+def image_content(location_url):
     try:
-        return requests.get(default_url).content
-    except requests.Timeout:
-        print("\nConnection Error.")
-        print("Does this script have access to internet and the URL?")
+        return requests.get(location_url).content
+    except Exception as e:
+        print(e)
         exit()
 
 
-# To download image with requests module
-def dl_image():
-    # The name of image file will be number sequence starting from 1
+def write_image(location, content):
+    # The name of the image will be a sequence of numbers starting from 1
     # e.g. "1.png", "2.png", etc.
     global count
-    img_name = f"{img_dir}\\{count}.png"
+    img_name = f"{IMG_DIR} {location}\\{count}.png"
 
     # Image writing operation
-    img_file = open(img_name, 'wb')
-    img_file.write(img_content())
-    img_file.close()
+    with open(img_name, 'wb') as img_file:
+        img_file.write(content)
 
-    print(f"Downloaded image as {count}.png at {datetime.now().strftime('%H:%M')}")
+    print(f"Image downloaded as {count}.png at {datetime.now().strftime('%H:%M')}")
     # Add the count so the filename doesn't collide
     count += 1
 
 
-# Get the hash of the site a.k.a. checking whether the image is renewed or not
-def get_hash():
-    return hashlib.sha256(img_content()).hexdigest()
+def get_hash(content):
+    return hashlib.sha256(content).hexdigest()
 
 
-# You can stop this script with CTRL + C or just simply exiting the command line.
-# Remember not to do that when a file is being written!
-def download_loop():
-    dl_image()
-    hash_now = get_hash()
+def run_downloader():
+    print("This is an automated script named BIgDoG (Basic Image Downloader for BMKG site).")
+    print("Default location: Tangerang.")
+    location = "Tangerang"
+    INTERVAL = 120 #in seconds
+    data = get_data()
+
+    if prompt_location():
+        print("Please choose one of the available locations: ")
+        print(", ".join(data.keys()))
+        while True:
+            location = input("Location: ").title()
+            if location in data: break
+            print("No valid location found! Please try again")
+    
+    location_url = data[location]
+    make_directory(location)
 
     try:
-        while 1:
-            if get_hash() != hash_now:
-                dl_image()
-                hash_now = get_hash()
-            time.sleep(t_interval)
+        content = image_content(location_url)
+        write_image(location, content)
+        current_hash = get_hash(content)
+
+        while True:
+            content = image_content(location_url)
+            temp_hash = get_hash(content)
+            if temp_hash != current_hash:
+                write_image(location, content)
+                current_hash = temp_hash
+                
+            time.sleep(INTERVAL)
 
     except KeyboardInterrupt:
-        print("\n\nKeyboard interrupt detected!\nNow closing...")
+        print("\nKeyboard interrupt detected!\nNow closing...")
         exit()
 
 
-# Run the function
-print_intro()
-make_directory()
-download_loop()
+run_downloader()
